140513e87ac24d4ddb63ef5fc893bc37b43fac76
N1Anoobis <mrbond84@hotmail.com>
2021-02-06 14:51:06 +0100

    Add navbar

--- src/main.js
+++ src/main.js
@@ -21,7 +21,25 @@ new Vue({
   router,
   template: `
     <div id="app">
-      <router-link to="/about">About</router-link>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-brand" href="#">vCustomers</div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+           <router-link class="nav-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
       <router-view></router-view>
   </div>
   `,
